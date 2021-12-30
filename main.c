9ae0c8d6864c9b9206219eb64c7c5c2c134f3ee8
iaraprezado5 <72602458+iaraprezado5@users.noreply.github.com>
2021-12-30 23:17:30 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 //Input is a file with the following structure (seperated by ;), where each line is a record. First line must be ignored.
 
+//Implemente um B+ Tree, assume registos de 708bytes e blocos (blocks) de 4Kbytes, a chave (key) é um inteiro de 32 bits (int32_t).
+
+//Input is a file with the following structure (seperated by ;), where each line is a record. First line must be ignored.
+
 #define MaxM 5 //size of the tree arrays
 #define MaxL 5 //size of linked list
 #include <stdio.h>
@@ -29,38 +33,69 @@ typedef struct record {
 typedef struct list_node{
     struct list_node* next;
     record* contents;
-    struct list_node* prev;
     uint32_t leaf;
 }list_node;
 
 typedef struct tree_node{
     uint32_t id;
     struct tree_node* child;
+    struct tree_node* parent;
     list_node* list;
     struct tree_node* next;
+    int *keys;
+
 }tree_node;
 
 int size_list(list_node * list_position){
-    int tamanho;
+    int size;
     while(list_position ->next != NULL){
-        tamanho++;
+        size++;
     }
-    tamanho++;
-    return tamanho;
+    size++;
+    return size;
 }
 
 int size_array(tree_node arr[]){
-    int tamanho_arr;
+    int size_arr;
     while(arr -> next != NULL){
         arr = arr -> next;
-        tamanho_arr++;
+        size_arr++;
     }
-    tamanho_arr++;
-    return tamanho_arr;
+    size_arr++;
+    return size_arr;
 }
+
+tree_node* insert_into_leaf(tree_node* leaf, record* node){
+    int i;
+    int insertion_point = 0;
+    while((insertion_point < leaf->id < leaf->keys[insertion_point+1]) && (leaf->keys[insertion_point] < leaf->keys[insertion_point+1])){
+        insertion_point++;
+    }
+    for(;leaf->id > insertion_point; i--){
+        leaf -> keys[leaf->id] = leaf -> keys[leaf->id -1];
+        leaf-> keys++;
+    }
+    leaf->keys[insertion_point] = node;
+    return leaf;
+}
+
+tree_node* make_leaf(void){
+    tree_node* new_leaf;
+    new_leaf = malloc(sizeof(tree_node));
+    if(new_leaf == NULL){
+        printf("Error");
+        return NULL;
+    }
+    new_leaf->keys = 0;
+    new_leaf->parent = NULL;
+    new_leaf->next = NULL;
+    return new_leaf;
+};
+
+
 int add_node(tree_node arr[], record* node){
     if(size_array(arr) < MaxM){
-        //adicionar node
+        insert_into_leaf(make_leaf(), node);
     }
     else{
         //split
@@ -120,15 +155,18 @@ record* read_file(FILE* fp) {
     fscanf(fp, " %d;%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%[^;];%d;%[^;];%d;%[^\n]", &node->id, node->firstname,
            node->surname, node->birthdate, node->died, node->Country, node->CountryCode, node->City,
            node->gender, &node->year, node->category, &node->share, node->motivation);
+    printf("%d", node);
     return node;
 }
 
 
+
 int main(int argc, char *argv[]) {
     FILE *fp = fopen("nobel_prizes_projeto.csv", "r");
-    printf("Size of struct; %lu\n", sizeof(record));
+    //printf("Size of struct; %lu\n", sizeof(record));
     insert_node(read_file(fp));
     return 0;
 }
+
 
 
