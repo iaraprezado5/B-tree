2d9c7967aabf09fb25b6b0be24297e1f4ba3da27
iaraprezado5 <72602458+iaraprezado5@users.noreply.github.com>
2021-12-31 00:46:16 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,6 @@
 
 //Input is a file with the following structure (seperated by ;), where each line is a record. First line must be ignored.
 
-//Implemente um B+ Tree, assume registos de 708bytes e blocos (blocks) de 4Kbytes, a chave (key) é um inteiro de 32 bits (int32_t).
-
-//Input is a file with the following structure (seperated by ;), where each line is a record. First line must be ignored.
-
-//Implemente um B+ Tree, assume registos de 708bytes e blocos (blocks) de 4Kbytes, a chave (key) é um inteiro de 32 bits (int32_t).
-
-//Input is a file with the following structure (seperated by ;), where each line is a record. First line must be ignored.
-
 #define MaxM 5 //size of the tree arrays
 #define MaxL 5 //size of linked list
 #define Meio 3
